1f683d6c66d6f69e02b7da6bd31cc71ceb94d789
Matej Mijoski <ma.mijoski@gmail.com>
2022-04-25 13:23:14 +0200

    Changed backend URL

--- src/service/CustomAxios.js
+++ src/service/CustomAxios.js
@@ -4,7 +4,7 @@ import {toast} from "react-toastify";
 
 const instance = axios.create({
     responseType: "json",
-    baseURL: process.env.BACKEND_URL || "http://127.0.0.1:5000/api",
+    baseURL: "https://emt-backend1.herokuapp.com/api",
     timeout: 5000,
     validateStatus: status => {
         if (status >= 500) {
